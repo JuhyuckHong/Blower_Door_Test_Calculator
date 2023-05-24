c744a910458fd2d9598ddc5948bd9712b65caf03
Juhyuck-Hong <juhyukhong@gmail.com>
2023-05-24 22:50:09 +0900

    Change function name and remove unnecessary module

--- measurement.py
+++ measurement.py
@@ -1,18 +1,18 @@
 import serial
 import crcmod.predefined
 import struct
-from simple_pid import PID
+import time
 
 
-def pressure_read(port='dev/ttyUSB0', baudrate=9600):
+def pressure_read(average_time, port='dev/ttyUSB0', baudrate=9600):
     # 시리얼 연결
     ser = serial.Serial(port=port,
                         baudrate=baudrate,
                         timeout=1)
-    
+    time.sleep(average_time)
     return ser
 
-def duty_control(duty, port='dev/ttyS0', baudrate=9600):
+def duty_set(duty, port='dev/ttyS0', baudrate=9600):
     # 시리얼 연결
     ser = serial.Serial(port=port,
                         baudrate=baudrate,
@@ -23,4 +23,4 @@ def duty_control(duty, port='dev/ttyS0', baudrate=9600):
 
 if __name__ == '__main__':
     pressure_read()
-    duty_control(10)
+    duty_set(0)
