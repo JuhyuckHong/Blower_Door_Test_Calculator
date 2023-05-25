a53d05d5e9e4ca59f7c9e419da3db46aed48c7ce
JuhyuckHong <juhyukhong@gmail.com>
2023-05-25 13:14:13 +0900

    Change module name and add converging conditions

--- pwm_pid_control.py
+++ pwm_pid_control.py
@@ -1,16 +1,19 @@
 import time
 from simple_pid import PID
-import measurement
+import sensor_and_controller
 
 def get_duty(target, delay, average_time, control_limit):
     # 현재 압력 값 측정 및 초기값 세팅
-    current = measurement.pressure_read(0.1)
+    current = sensor_and_controller.pressure_read(0.1)
     duty = 0
 
-    # 수렴 조건
+    # 압력 수렴 조건
     convergence_time = 0
-    convergence_threshold = target/10
+    pressure_threshold = target/10
     duration = 10
+    # duty 수렴 조건
+    window = []
+    window_size = 50
 
     # 실패 조건
     failure_time = 0
@@ -33,36 +36,46 @@ def get_duty(target, delay, average_time, control_limit):
         duty += control 
         duty = max(0, min(100, duty))
         # duty 값 적용
-        measurement.duty_set(duty)
+        sensor_and_controller.duty_set(duty)
         # 압력 변화 대기
         time.sleep(delay)
         # 압력 값 측정
-        current = measurement.pressure_read(average_time)
+        current = sensor_and_controller.pressure_read(average_time)
+        # duty의 이동 평균 계산
+        window.append(duty)
+        if len(window) > window_size:
+            window = window[1:]
+
+        duty_avg = sum(window)/len(window)
+
         # 제어 종료 시간
         time_diff = time.time() - time_start
-        # 오차
-        error = abs(target - current)
-        if error < convergence_threshold:
+        # 압력 오차
+        error_pressure = abs(target - current)
+        # duty 오차
+        error_duty = abs(duty_avg - duty)
+
+        if error_pressure < pressure_threshold and error_duty < max(2, duty/10):
             convergence_time += time_diff
         else:
             convergence_time = 0
         
         if convergence_time >= duration:
-            current = measurement.pressure_read(final_measure_time)
+            current = sensor_and_controller.pressure_read(final_measure_time)
             return (duty, True, current)
 
         # duty 100으로 설정해도 목표 압력에 도달하지 못하는 경우
-        if duty == 100 and error > failure_threshold:
+        if duty == 100 and error_pressure > failure_threshold:
             failure_time += time_diff
         else:
             failure_time = 0
 
         # duty 0으로 설정해도 목표 압력 값을 초과하는 경우
-        if duty == 0 and error > failure_threshold:
+        if duty == 0 and error_pressure > failure_threshold:
             failure_time += time_diff
         else:
             failure_time = 0
 
         if failure_time >= duration:
-            current = measurement.pressure_read(final_measure_time)
+            current = sensor_and_controller.pressure_read(final_measure_time)
             return (duty, False, current)
